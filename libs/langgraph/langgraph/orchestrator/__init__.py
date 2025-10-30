7a63ff280729a951a93bb45467b5bd82bda54ed7
gnixdz <gnixdz@gmail.com>
2025-10-30 10:47:04 +0100

    Update __init__.py

--- libs/langgraph/langgraph/orchestrator/__init__.py
+++ libs/langgraph/langgraph/orchestrator/__init__.py
@@ -1,4 +1,4 @@
 from .policy import load_policy, eval_pred
-from .registry import NodeSpec, NodeRegistry, node_meta
+from .registry import NodeSpec, NodeRegistry, node_meta, get_global_registry
 from .controller import Controller
 from .orchestrator import Orchestrator
